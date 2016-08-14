5ea94b1593825f459a5072970c2f516bff997c1a
Alex Carlson <alexcarlsongraphics@gmail.com>
2016-08-14 11:43:24 -0500

    init

---
+++ Assets/followPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followPlayer : MonoBehaviour {
+
+    public Transform target;
+    float lerpSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+        if (target == null)
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.transform.position + (target.transform.forward * -5f) + (target.transform.up * 2f);
+        transform.LookAt(target);
+	}
+}
---
+++ Assets/playerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+    public float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            transform.Rotate(Vector3.up * 90);
+        }
+	}
+
+    void FixedUpdate()
+    {
+        transform.position = transform.position + (transform.forward * speed * Time.deltaTime);
+    }
+}
